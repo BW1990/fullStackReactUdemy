07962ea5b84aef75741b5110d5066eab0d79c1e8
Brian Winward <brianw@evokesolutions.com>
2017-07-28 16:50:51 -0400

    finished auth flow

---
+++ services/passport.js
@@ -0,0 +1,49 @@
+// passportJs
+const passport = require('passport');
+
+// passport strategy Google OAuth 2.0
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const mongoose = require('mongoose');
+
+// get keys
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+});
+
+// console.developers.google.com
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // already have record with given profile ID
+            done(null, existingUser);
+          } else {
+            // don't have user record with this ID, make new record
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        }
+      );
+    }
+  )
+);
