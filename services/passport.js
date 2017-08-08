25d5e078e0b5368f2790e3f37fa02ad3f2a373a9
Brian Winward <brianw@evokesolutions.com>
2017-08-08 10:11:56 -0400

    Added building and client side app

--- services/passport.js
+++ services/passport.js
@@ -31,20 +31,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // already have record with given profile ID
-            done(null, existingUser);
-          } else {
-            // don't have user record with this ID, make new record
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        }
-      );
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // already have record with given profile ID
+        return done(null, existingUser);
+      }
+      // don't have user record with this ID, make new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
